12b3e3067b20b3181e3ad5fee4145aaac3cbacfe
albernt <albertobernet@gmail.com>
2025-01-24 22:59:58 +0100

    Retoques

--- src/main/java/DAO/AnimalDAO.java
+++ src/main/java/DAO/AnimalDAO.java
@@ -13,7 +13,7 @@ public interface AnimalDAO {
      *
      * @param animal El objeto Animal a guardar.
      */
-    void save(Animal animal);  // Create
+    void save(Animal animal);
 
     /**
      * Obtiene un objeto Animal por su ID.
@@ -21,7 +21,7 @@ public interface AnimalDAO {
      * @param id El ID del Animal a buscar.
      * @return El objeto Animal correspondiente al ID, o null si no se encuentra.
      */
-    Animal findById(Long id); // Read
+    Animal findById(Long id);
 
     /**
      * Obtiene una lista de animales por su especie.
@@ -29,26 +29,26 @@ public interface AnimalDAO {
      * @param especie La especie de los animales a buscar.
      * @return Una lista de objetos Animal que pertenecen a la especie indicada.
      */
-    List<Animal> findByEspecie(String especie); // Read by especie
+    List<Animal> findByEspecie(String especie);
 
     /**
      * Obtiene todos los animales de la base de datos.
      *
      * @return Una lista con todos los objetos Animal almacenados en la base de datos.
      */
-    List<Animal> findAll();   // Read all
+    List<Animal> findAll();
 
     /**
      * Actualiza un objeto Animal en la base de datos.
      *
      * @param animal El objeto Animal con los nuevos valores.
      */
-    void update(Animal animal); // Update
+    void update(Animal animal);
 
     /**
      * Elimina un objeto Animal de la base de datos.
      *
      * @param animal El objeto Animal a eliminar.
      */
-    void delete(Animal animal); // Delete
+    void delete(Animal animal);
 }
